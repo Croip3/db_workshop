c14f7d4294751fd841d59c5c1f87e9452e1711df
Christopher <37404705+Croip3@users.noreply.github.com>
2021-12-01 11:33:29 +0100

    Update firebase_config.js

--- restaurant_dev/src/components/firebase_config.js
+++ restaurant_dev/src/components/firebase_config.js
@@ -3,22 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAMrbhIR8lJRv2_rZKmzY-aIG4qZPI8pKY",
-
-  authDomain: "restaurant-authentication.firebaseapp.com",
-
-  //databaseURL: "https://restaurant-authentication-default-rtdb.europe-west1.firebasedatabase.app",
-
-  projectId: "restaurant-authentication",
-
-  storageBucket: "restaurant-authentication.appspot.com",
-
-  messagingSenderId: "477340841738",
-
-  appId: "1:477340841738:web:453529d511fe7ff961acf3",
-
-  measurementId: "G-RDBT5GSYR9"
-
 
 };
 
